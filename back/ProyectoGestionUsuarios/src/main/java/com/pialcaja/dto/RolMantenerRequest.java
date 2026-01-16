27348fd519c74e4fa19d312ecc40d4cfb6929c88
pialcaja <pialcaja@gmail.com>
2026-01-15 23:17:21 -0500

    Construcción API Roles

---
+++ back/ProyectoGestionUsuarios/src/main/java/com/pialcaja/dto/RolMantenerRequest.java
@@ -0,0 +1,10 @@
+package com.pialcaja.dto;
+
+import lombok.Data;
+
+@Data
+public class RolMantenerRequest {
+
+	private String nombre;
+	private String descripcion;
+}
--- back/ProyectoGestionUsuarios/src/main/java/com/pialcaja/repository/RolRepository.java
+++ back/ProyectoGestionUsuarios/src/main/java/com/pialcaja/repository/RolRepository.java
@@ -2,13 +2,25 @@ package com.pialcaja.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.pialcaja.model.Rol;
 
 @Repository
 public interface RolRepository extends JpaRepository<Rol, Long> {
+
+	@Query("""
+			    SELECT r FROM Rol r
+			    WHERE UPPER(u.nombre) LIKE UPPER(CONCAT('%', :filtro, '%'))
+			""")
+	Page<Rol> buscarPorFiltro(@Param("filtro") String filtro, Pageable pageable);	
+
+	Boolean existsByNombreAndIdNot(String nombre, Long id);
 
 	Optional<Rol> findByNombre(String nombre);
 }
